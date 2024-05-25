188f2e8c93a1745663659117ceb28c50563dc263
Kalveir <yogakrisna567@gmail.com>
2024-05-25 18:59:07 +0700

    upadate : tabel data

--- tugas2/todolist/urls.py
+++ tugas2/todolist/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpaterns = [
 	path('',views.index, name='todo-index'),
 	path('add/',views.add,name='todo-add'),
-	path('update/<int:todo_id>',views.update, name='todo-update'),
-	path('delete/int:todo_id>',views.delete, name='todo-delete'),
+	# path('update/<int:todo_id>',views.update, name='todo-update'),
+	# path('delete/int:todo_id>',views.delete, name='todo-delete'),
 ]
